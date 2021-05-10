cbe9bccd331bd5bce4fef71e0fb4327dbbfcf925
dejanagrujic <39495830+dejanagrujic@users.noreply.github.com>
2021-05-10 22:24:57 +0200

    Updating dates

--- script.js
+++ script.js
@@ -17,9 +17,9 @@ const account1 = {
     '2020-01-28T09:15:04.904Z',
     '2020-04-01T10:17:24.185Z',
     '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+	'2021-05-04T17:01:17.194Z',
+    '2021-05-08T23:36:17.929Z',
+    '2021-05-09T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -103,6 +103,26 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const formatMovementDate = function (date) {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+  const daysPassed = calcDaysPassed(new Date(), date);
+  console.log(daysPassed);
+
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+  else {
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+    //const hour = `${now.getHours()}`.padStart(2, 0);
+    //const min = `${now.getMinutes()}`.padStart(2, 0);
+    return `${day}/${month}/${year}`;
+  }
+};
+
 const displayMovements = function (currAcc, sort = false) {
   containerMovements.innerHTML = '';
   
@@ -115,12 +135,7 @@ const displayMovements = function (currAcc, sort = false) {
     //console.log(type);
 	
 	const date = new Date(acc.movementsDates[i]);
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0); //because of zero based
-    const year = date.getFullYear();
-    //const hour = `${now.getHours()}`.padStart(2, 0);
-    //const min = `${now.getMinutes()}`.padStart(2, 0);
-    const displayDate = `${day}/${month}/${year}`;
+    const displayDate = formatMovementDate(date);
 
     const html = `
     <div class="movements__row">
